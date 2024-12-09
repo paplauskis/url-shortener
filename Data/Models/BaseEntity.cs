a3f55478040ad1ec8adc6d27e80ad354e68c7a23
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-09 19:10:53 +0200

    create base model

---
+++ Data/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace url_shortener.Data.Models;
+
+public abstract class BaseEntity
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("created_at")]
+    public DateTime CreatedAt { get; private set; }
+
+    protected BaseEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
+}
