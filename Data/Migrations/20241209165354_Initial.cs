5963a945138ec2b068eaba224deee3ca0d0d37be
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-09 19:11:57 +0200

    add migration

---
+++ Data/Migrations/20241209165354_Initial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace url_shortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "url_entity",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    original_url = table.Column<string>(type: "text", nullable: false),
+                    shortened_url = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_url_entity", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "url_access_log",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    url_entity_id = table.Column<int>(type: "integer", nullable: false),
+                    accessed_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ip_address = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    user_agent = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_url_access_log", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_url_access_log_url_entity_url_entity_id",
+                        column: x => x.url_entity_id,
+                        principalTable: "url_entity",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_url_access_log_url_entity_id",
+                table: "url_access_log",
+                column: "url_entity_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "url_access_log");
+
+            migrationBuilder.DropTable(
+                name: "url_entity");
+        }
+    }
+}
