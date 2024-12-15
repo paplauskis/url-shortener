6b6e6d5d0e9cd07b953914931742d67b205b9c63
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-15 21:14:24 +0200

    added endpoint to create short urls

--- Controllers/UrlController.cs
+++ Controllers/UrlController.cs
@@ -1,10 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using url_shortener.Services;
 
 namespace url_shortener.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/api")]
 public class UrlController : ControllerBase
 {
+    private readonly UrlEntityService _urlEntityService;
+
+    public UrlController(UrlEntityService urlEntityService)
+    {
+        _urlEntityService = urlEntityService;
+    }
+    
+    [HttpPost("/shorten/{url}")]
+    public async Task<IActionResult> CreateNewUrl([FromRoute] string url)
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            return BadRequest("URL is required");
+        }
+
+        var newUrl = await _urlEntityService.CreateUrlEntity(url);
+        
+        return Ok(newUrl);
+    }
     
 }
