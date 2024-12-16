9a3c6f84531dc71b8091debf0a68531aefd6575e
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-16 20:47:52 +0200

    removed ClickCount property

--- Domain/DTOs/UrlEntityDto.cs
+++ Domain/DTOs/UrlEntityDto.cs
@@ -4,12 +4,10 @@ public class UrlEntityDto
 {
     public string OriginalUrl { get; init; }
     public string ShortenedUrl { get; init; }
-    public int ClickCount { get; set; }
 
-    public UrlEntityDto(string originalUrl, string shortenedUrl, int clickCount = 0)
+    public UrlEntityDto(string originalUrl, string shortenedUrl)
     {
         OriginalUrl = originalUrl;
         ShortenedUrl = shortenedUrl;
-        ClickCount = clickCount;
     }
 }
