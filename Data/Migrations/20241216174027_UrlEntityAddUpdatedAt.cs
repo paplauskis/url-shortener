51718b1ff6ad7948277b4aac0902b72e7e5fde42
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-16 19:59:50 +0200

    new migration: add UpdatedAt property to UrlEntity

---
+++ Data/Migrations/20241216174027_UrlEntityAddUpdatedAt.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace url_shortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UrlEntityAddUpdatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "shortened_url",
+                table: "url_entity",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "original_url",
+                table: "url_entity",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "url_entity",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "url_entity");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "shortened_url",
+                table: "url_entity",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "original_url",
+                table: "url_entity",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}
