14cd6837317eec7000ee5747abb79344ad256d3a
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-17 11:10:24 +0200

    added method that saves request info

--- Services/UrlAccessLogService.cs
+++ Services/UrlAccessLogService.cs
@@ -1,4 +1,6 @@
 using url_shortener.Data.Repositories;
+using UAParser;
+using url_shortener.Domain.Models;
 
 namespace url_shortener.Services;
 
@@ -9,5 +11,21 @@ public class UrlAccessLogService
     public UrlAccessLogService(UrlAccessLogRepository urlAccessLogRepository)
     {
         _repository = urlAccessLogRepository;
+    }
+
+    public async Task SaveRequestInfo(string userAgent, string ip, int urlId, DateTime accessDate)
+    {
+        var parser = Parser.GetDefault();
+        var clientInfo = parser.Parse(userAgent);
+
+        var entity = new UrlAccessLog(
+            urlId,
+            ip,
+            clientInfo.Device.Family,
+            clientInfo.OS.ToString(),
+            clientInfo.UA.ToString()
+        );
+        
+        await _repository.AddAsync(entity);
     }
 }
