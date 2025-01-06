ed5e14c3daa68cbac682cfaccbac5c4d344bf124
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-01-06 16:37:48 +0200

    add user table

---
+++ Data/Migrations/20250106143558_add-user.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace url_shortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "user_id",
+                table: "url_entity",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_url_entity_user_id",
+                table: "url_entity",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_url_entity_Users_user_id",
+                table: "url_entity",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_url_entity_Users_user_id",
+                table: "url_entity");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_url_entity_user_id",
+                table: "url_entity");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "url_entity");
+        }
+    }
+}
