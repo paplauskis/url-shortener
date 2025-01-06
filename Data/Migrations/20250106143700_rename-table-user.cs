2cba276bde1b31facf5918afab6ef19d1e8541eb
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-01-06 16:38:41 +0200

    rename table 'Users' to 'user'

---
+++ Data/Migrations/20250106143700_rename-table-user.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace url_shortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class renametableuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_url_entity_Users_user_id",
+                table: "url_entity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "user");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user",
+                table: "user",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_url_entity_user_user_id",
+                table: "url_entity",
+                column: "user_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_url_entity_user_user_id",
+                table: "url_entity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user",
+                table: "user");
+
+            migrationBuilder.RenameTable(
+                name: "user",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_url_entity_Users_user_id",
+                table: "url_entity",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
