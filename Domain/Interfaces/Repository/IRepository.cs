d94304c952dce00ab41081d0bc78d9eaf01c0e14
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-01-06 19:50:02 +0200

    remove GetAllAsync method

--- Domain/Interfaces/Repository/IRepository.cs
+++ Domain/Interfaces/Repository/IRepository.cs
@@ -4,7 +4,6 @@ namespace url_shortener.Domain.Interfaces.Repository;
 
 public interface IRepository<T> where T : BaseEntity
 {
-    Task<List<T>> GetAllAsync();
     Task<T> GetByIdAsync(int id);
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T updatedEntity);
