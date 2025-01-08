495e1295dc49e181238901ebd1beef594dc1e644
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-01-08 20:53:57 +0200

    removed one contructor and Id property

--- Domain/Exceptions/UserAlreadyExistsException.cs
+++ Domain/Exceptions/UserAlreadyExistsException.cs
@@ -3,7 +3,6 @@ namespace url_shortener.Domain.Exceptions;
 public class UserAlreadyExistsException : Exception
 {
     public string Email { get; init; }
-    public int Id { get; init; }
     
     public UserAlreadyExistsException(string message) : base(message) {}
 
@@ -11,11 +10,5 @@ public class UserAlreadyExistsException : Exception
         : base(message)
     {
         Email = email;
-    }
-
-    public UserAlreadyExistsException(string message, int id)
-        : base(message)
-    {
-        Id = id;
     }
 }
