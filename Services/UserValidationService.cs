010e08932cd198a91f8e4028b3bcad8688b2a9c1
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-01-10 14:33:54 +0200

    created class that validates LoginUserRequestDto using validators

---
+++ Services/UserValidationService.cs
@@ -0,0 +1,30 @@
+using Microsoft.IdentityModel.Tokens;
+using url_shortener.Domain.DTOs;
+using url_shortener.Domain.Interfaces.Repository;
+using url_shortener.Domain.Models;
+
+namespace url_shortener.Services;
+
+public class UserValidationService
+{
+    private readonly IEnumerable<IValidator<LoginUserRequestDto>> _validators;
+
+    public UserValidationService(IEnumerable<IValidator<LoginUserRequestDto>> validators)
+    {
+        _validators = validators;
+    }
+
+    public bool Validate(LoginUserRequestDto user)
+    {
+        
+        foreach (var validator in _validators)
+        {
+            if (!validator.IsValid(user))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
